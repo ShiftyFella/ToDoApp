ec7b1842ec45af6451de01cf0930ac7dae4a6836
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2018-01-03 01:26:00 -0500

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Viktor Bilyk on 2018-01-03.
+//  Copyright © 2018 Shifty Land LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
