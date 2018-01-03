16329b0a2095f24c5ce7d3570151089ceb5b3c06
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2018-01-03 02:47:40 -0500

    initial set up

---
+++ ToDoApp/DetailsViewViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MAPD 714 - Assigment 2 - ToDo List Application
+//
+// Name: Todo List
+// Desc: To do app
+// Contributors: #300964200 - Viktor Bilyk
+//               #300965775 - Timofei Sopin
+//
+// Ver: 0.1 - Basic Set Up
+// File: Details Screen View Controller handler
+
+
+import UIKit
+
+class DetailsViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -1,23 +1,66 @@
 //
-//  ViewController.swift
-//  ToDoApp
+//  MAPD 714 - Assigment 2 - ToDo List Application
 //
-//  Created by Viktor Bilyk on 2018-01-03.
-//  Copyright © 2018 Shifty Land LLC. All rights reserved.
+// Name: Todo List
+// Desc: To do app
+// Contributors: #300964200 - Viktor Bilyk
+//               #300965775 - Timofei Sopin
 //
+// Ver: 0.1 - Basic Set Up
+// File: Main Screen View Controller handler
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var tasksList = [NSManagedObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func addTask(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "To Do Task",
+                                      message: "Add new task",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Add",
+                                       style: .default) {
+                                        [weak self] action in
+                                        
+                                        guard let textField = alert.textFields?.first,
+                                            let nameOfTask = textField.text else {
+                                                return
+                                        }
+                                        
+                                        //add task to tasks
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel)
+        
+        alert.addTextField()
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return tasksList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")! as UITableViewCell
+        //assign cell text
+        return cell
     }
 
 
